870d680f4535faf8342640aad42697e5d58b105c
Zach Weinberg <weinberz@gmail.com>
2014-01-13 14:58:48 -0500

    Added thresholding to measurement

--- loadImage.m
+++ loadImage.m
@@ -23,21 +23,31 @@ CFP = zeros(512,512,num_frames);
 FRET = zeros(512,512,num_frames);
 YFP = zeros(512,512,num_frames);
 Tag = zeros(512,512,num_frames);
+
+gaussian_filter = fspecial('gaussian',[6 6],2);
 % Disable warning from 
 warning('off','MATLAB:imagesci:tiffmexutils:libtiffErrorAsWarning')
 for i=1:num_frames
-    CFP(:,:,i) = imread([p f],'Index',CFPch+(i-1)*num_channels);
-    FRET(:,:,i) = imread([p f],'Index',FRETch+(i-1)*num_channels);
-    YFP(:,:,i) = imread([p f],'Index',YFPch+(i-1)*num_channels);
-    Tag(:,:,i) = imread([p f],'Index',Tagch+(i-1)*num_channels);
+    CFP(:,:,i) = imfilter(imadjust(mat2gray(imread([p f],'Index',CFPch+(i-1)*num_channels))),gaussian_filter,'same');
+    FRET(:,:,i) = imfilter(imadjust(mat2gray(imread([p f],'Index',FRETch+(i-1)*num_channels))),gaussian_filter,'same');
+    YFP(:,:,i) = imfilter(imadjust(mat2gray(imread([p f],'Index',YFPch+(i-1)*num_channels))),gaussian_filter,'same');
+    Tag(:,:,i) = imfilter(imadjust(mat2gray(imread([p f],'Index',Tagch+(i-1)*num_channels))),gaussian_filter,'same');
 end
 warning('on','all')
 
 CFPm = squeeze(mean(CFP,3));
-CFPm = (CFPm-min(CFPm(:)))/(max(CFPm(:))-min(CFPm(:)));
 
 bgMask = roipoly(CFPm);
-fgMask = roipoly(CFPm);
+
+CFP = CFP-mean(CFP(bgMask));
+CFP(CFP<0)=0;
+YFP = YFP-mean(YFP(bgMask));
+YFP(YFP<0)=0;
+FRET = FRET-mean(FRET(bgMask));
+FRET(FRET<0)=0;
+Tag = Tag-mean(Tag(bgMask));
+Tag(Tag<0)=0;
+
 
 data.cfp = zeros(num_frames,1);
 data.yfp = zeros(num_frames,1);
@@ -49,10 +59,13 @@ for i=1:num_frames
 	frameFRET = FRET(:,:,i);
 	frameTag = Tag(:,:,i);
 	
-    data.cfp(i) = mean(frameCFP(fgMask))-mean(frameCFP(bgMask));
-	data.yfp(i) = mean(frameYFP(fgMask))-mean(frameYFP(bgMask));
-	data.fret(i) = mean(frameFRET(fgMask))-mean(frameFRET(bgMask));
-	data.tag(i) = mean(frameTag(fgMask))-mean(frameTag(bgMask));
+    data.cfp(i) = mean(frameCFP(frameCFP ~= 0));
+	data.yfp(i) = mean(frameYFP(frameYFP ~= 0));
+	data.fret(i) = mean(frameFRET(frameFRET ~= 0));
+	data.tag(i) = mean(frameTag(frameTag ~= 0));
 end
 
-data.ratio = data.fret./data.cfp
+data.ratio = data.fret./data.cfp;
+
+figure()
+plot(data.ratio)
